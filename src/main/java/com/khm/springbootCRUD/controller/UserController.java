6631a4eb8fa45f9b53c7b77ae3e445be44010e2b
HyeongMin Kim <92067099+fkgnssla@users.noreply.github.com>
2022-08-06 13:31:02 +0900

    [controller] UserController 추가

---
+++ src/main/java/com/khm/springbootCRUD/controller/UserController.java
@@ -0,0 +1,56 @@
+package com.khm.springbootCRUD.controller;
+
+import com.khm.springbootCRUD.model.User;
+import com.khm.springbootCRUD.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
+
+@Controller
+@RequiredArgsConstructor
+public class UserController {
+
+    private final UserService userService;
+
+    @GetMapping("/")
+    public String home() {
+        return "createForm";
+    }
+
+    @PostMapping("/")
+    public String createUser(@ModelAttribute User user) {
+        userService.createUser(user);
+        return "redirect:/";
+    }
+
+    @GetMapping("/users")
+    public String users(Model model) {
+        List<User> users = userService.findUsers();
+        model.addAttribute("users",users);
+        return "users";
+    }
+
+    @GetMapping("/user/edit/{id}")
+    public String editUser(@PathVariable("id") Long id, Model model) {
+        model.addAttribute("user", userService.findUser(id));
+        return "editUser";
+    }
+
+    @PostMapping("/user/edit")
+    public String edit(@ModelAttribute("user") User user) {
+        userService.updateUser(user);
+        return "redirect:/users";
+    }
+
+    @GetMapping("/user/delete/{id}")
+    public String deleteUser(@PathVariable("id") Long id) {
+        userService.deleteUser(id);
+        return "redirect:/users";
+    }
+}
